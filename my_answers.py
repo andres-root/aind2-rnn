80983daf58f921f992f36eca681b390917fcaa85
Andres Root <info@andresroot.co>
2017-09-30 22:50:56 -0500

    Working on RNN project

--- my_answers.py
+++ my_answers.py
@@ -6,41 +6,52 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    w = window_size
+    for i in range(len(series)):
+        if w == len(series):
+            break
+        if i > 0:
+            w += 1
+        X.append(series[i:w])
+        y.append(series[w:w + 1])
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y.shape = (len(y), 1)
 
-    return X,y
+    return X, y
+
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
     pass
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+# TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
 
     return text
 
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+
+# TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
 
-    return inputs,outputs
+    return inputs, outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
